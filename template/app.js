9efc4822e0b482456efcf68d153cf9d2e2b70f91
bdm-k <kokusyunn@gmail.com>
2020-03-28 18:09:49 +0900

    have cumulative hold every minutes' data.

--- template/app.js
+++ template/app.js
@@ -1,48 +1,121 @@
 "use strict";
 
-// initlize variable:curretobj in getCumulativeSales
-function init(e, l) {
+function gapOfMinutues (x, y) {
+    let gap = (x >= y) ? x - y : 0
+    gap /= 60000;
+    gap = Math.floor(gap);
+    return gap
+}
+function initCurrentobj (event, numOfItems) {
     let obj = {
-        time: e.time,
+        time: event.time,
         data: []
     }
-    for (let i = 0; i < l; i++) {
+    for (let i = 0; i < numOfItems; i++) {
         obj.data.push({
             nums: 0,
             sales: 0
         });
     }
-    obj.data[e.item_id].nums = e.num;
-    obj.data[e.item_id].sales = e.num*e.price;
+    obj.data[event.item_id].nums = event.num;
+    obj.data[event.item_id].sales = event.num*event.price;
     return obj;
 }
-
-// recieve sales.json'object, return [object].
-// look over Pull Request for the account of getCumulativeSales.
-function getCumulativeSales(unprocessedobj) {
-    let item_length = unprocessedobj.items.length, sales = unprocessedobj.sales;
-    let cumulative = [], currentobj;
-    if (sales.length === 1) {
-        return [init(sales[0], item_length)]
-    }
-    for (let i = 0; i < sales.length; i++) {
-        let event = sales[i]
-        if (!currentobj) {
-            currentobj = init(event, item_length);
-            continue;
-        }
-        if ((new Date(event.time)).getMinutes() !== (new Date(currentobj.time)).getMinutes()) {
+function putTogether (arr, type) {
+    return arr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue[type];
+    }, 0);
+}
+function setRightNow (vueInstance) {
+    let now = new Date();
+    vueInstance.rightNow = `${now.getDate()} ${now.getHours()}:${now.getMinutes()}`
+}
+function setUsers (vueInstance, users) {
+    vueInstance.users = users;
+}
+function setProductNames (vueInstance, items) {
+    vueInstance.productNames = items.map(item => {
+        return item.name;
+    });
+}
+// the picture of currentobj
+// {
+//     time: 
+//     data: [
+//         {
+//             nums: 
+//             sales:
+//         },
+//         {
+//             nums:
+//             sales:
+//         }
+//         ...objects as many as the number of items
+//     ]
+// }
+function setCumulative (vueInstance, sales) {
+    sales.push({
+        time: Date.now(),
+        item_id: 0,
+        num: 0,
+        price: 0
+    });
+    let numOfItems = vueInstance.productNames.length;
+    let cumulative = []
+    let currentobj = initCurrentobj(sales[0], numOfItems);
+    let pack = (gap, event) => {
+        if (gap === 0) {
+            let {item_id ,num, price} = event;
+            currentobj.data[item_id].nums += num;
+            currentobj.data[item_id].sales += num*price;
+        } else {
             cumulative.push(JSON.parse(JSON.stringify(currentobj)));
+            currentobj.time += 60000;
+            update_pushCurrentObj(gap - 1, event);
         }
-        currentobj.time = event.time;
-        currentobj.data[event.item_id].nums += event.num;
-        currentobj.data[event.item_id].sales += event.num*event.price;
     }
-    cumulative.push(Object.assign({}, currentobj));
-    return cumulative
+    for (let event of sales.slice(1)) {
+        let {is_uriko, num, price, user_id} = event;
+        if (is_uriko) {
+            vueInstance.users[user_id].sales += num*price;
+        }
+        let gap = gapOfMinutues(event.time, currentobj.time);
+        pack(gap, event);
+    }
+    cumulative.push(JSON.parse(JSON.stringify(currentobj)));
+    vueInstance.cumulative = cumulative;
+}
+function setProductDetails (vueInstance) {
+    vueInstance.productDetails = [];
+    let lastData = vueInstance.cumulative[vueInstance.cumulative.length - 1].data;
+    for (let i = 0; i < lastData.length; i++) {
+        vueInstance.productDetails.push({
+            id: i,
+            sales: lastData[i].sales,
+            nums: lastData[i].nums
+        });
+    }
+    vueInstance.productDetails.push({
+        id: lastData.length,
+        sales: putTogether(lastData, 'sales'),
+        nums: putTogether(lastData, 'nums'),
+    });
+}
+function setUserRanking (vueInstance) {
+    let extraction = [];
+    for (let key in vueInstance.users) {
+        extraction.push([key, vueInstance.users[key].sales ]);
+    }
+    extraction.sort((x, y) => {
+        return (x[1] >= y[1]) ? -1 : 1
+    })
+    vueInstance.userRanking = extraction.map(value => {
+        return value[0];
+    })
 }
 
-// makeGraphData makes data for the graph from the caumulative, which is return-value of getcumulativeSales.
+
+// makeGraphData makes data for the graph from the caumulative, which is return-value of setCumulative.
 function makeGraphData(cumulative, f) {
     let graphdata = [[], []];
     for (let i = 0; i < cumulative.length; i++) {
@@ -121,12 +194,19 @@ function getJson() {
     return axios.get('https://potipoti.herokuapp.com/').then(response => response.data);
 }
 
-// property maker for 'ALL'.
-function putTogether(arr, type) {
-    return arr.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue[type];
-    }, 0);
-} 
+function vueInitlizer(unprocessedobj) {
+    let {items, users, sales} = unprocessedobj;
+    let vueInstance = {};
+    setRightNow(vueInstance);
+    setUsers(vueInstance, users);
+    setProductNames(vueInstance, items);
+    vueInstance.productNames.push('ALL');
+    setCumulative(vueInstance, sales);
+    setProductDetails(vueInstance);
+    setUserRanking(vueInstance);
+    vueInstance.selectedProductIds = [items.length];
+    return vueInstance;
+}
 
 
 
@@ -137,19 +217,44 @@ var vm = new Vue({
     data: {
         // rightNow represents time
         rightNow: '',
+
         // products ids which is shown in the graph.
         selectedProductIds: [],
-        // The index in this arr will be its productId.
+
+        // The index in this arr will be its productId. 'ALL' is the last.
         productNames: [],
+
 // NOTE: colors needs to be set Manually.
         // each element of productColors is composed of [backgroundColor, borderColor]. This is for graph.
         productColors: [["rgba(30, 144, 255, 0.15)", "rgba(0, 128, 255, 1)"], ["rgba(143, 255, 31, 0.15)", "rgba(143, 255, 31, 1)"], ["rgba(255, 31, 143, 0.15)" ,"rgba(255, 31, 143, 1)"]],
+
         // each element of productDetails is composed of {id, sales, nums}
         productDetails: [],
-        // data made by getCumulativeSales.
+
+        // data made by setCumulative.
         cumulative: [],
+
         // instance of Chart.
         myChart: null,
+
+        // user information
+        // {
+        //     userId: {
+        //         user_name: 
+        //         photo:
+        //         sales:
+        //     },
+        //     userId: {
+        //         userName:
+        //         photo:
+        //         sales:
+        //     },
+        //     ...
+        // }
+        users: {},
+
+        // user id will be set in deceding order about their sales.
+        userRanking: []
     },
     methods: {
 
@@ -199,46 +304,21 @@ var vm = new Vue({
     mounted() {
         getJson()
         .then(obj => {
-            // initlize productNames
-            for(let i = 0; i < obj.items.length; i++) {
-                this.productNames.push(obj.items[i].name);
-            }
-            this.productNames.push('ALL');
-
-            // initlize cumulative
-            this.cumulative = getCumulativeSales(obj);
-
-            // initlize rightNow
-            let last = this.cumulative[this.cumulative.length - 1];
-            let d = new Date(last.time);
-            this.rightNow = `${d.getDate()} ${d.getHours()}:${d.getMinutes()}`;
-
-            // initialize productDetails
-            for (let i = 0; i < last.data.length; i++) {
-                this.productDetails.push(
-                    {
-                        id: i,
-                        sales: last.data[i].sales,
-                        nums: last.data[i].nums,
-                    }
-                );
-            }
-            this.productDetails.push({
-                id: last.data.length,
-                sales: putTogether(last.data, 'sales'),
-                nums: putTogether(last.data, 'nums'),
-            });
-
+            let {rightNow, selectedProductIds, productNames, productDetails, cumulative, users, userRanking} = vueInitlizer(obj);
+            this.rightNow = rightNow;
+            this.selectedProductIds = selectedProductIds;
+            this.productDetails = productDetails;
+            this.productNames = productNames;
+            this.cumulative = cumulative;
+            this.users = users;
+            this.userRanking = userRanking;
             // initlize myChart
-            let graphdata = makeGraphData(this.cumulative, x => {
+            let graphdata = makeGraphData(cumulative, x => {
                 return x.reduce((accumulator, currentValue) => {
                     return accumulator + currentValue.sales
                 }, 0);
             });
             this.myChart = new Chart(document.getElementById('myChart'), initlizeGraphObj(graphdata[0], graphdata[1], 'ALL', this.productColors[last.data.length]));
-
-            // intialize selectedProductIds
-            this.selectedProductIds.push(last.data.length);
         })
         .catch(error => {
             console.error(error);
@@ -246,37 +326,23 @@ var vm = new Vue({
     }
 });
 
-// update vm.cumulative, rightNow, productDetails, and myChart every 60 seconds.
+// update every 60 seconds.
 setInterval(() => {
     getJson()
     .then(obj => {
-        let latest = getCumulativeSales(obj);
-        let f = x => {
-            return (new Date(x[x.length - 1].time)).getMinutes();
-        }
-        if (f(vm.cumulative) === f(latest)) {
-            return Promise.resolve(null)
-        }
-        vm.cumulative = latest
-        let last = vm.cumulative[vm.cumulative.length - 1];
+        let {rightNow, selectedProductIds, productNames, productDetails, cumulative, users, userRanking} = vueInitlizer(obj);
+        vm.rightNow = rightNow;
+        vm.rightNow = rightNow;
+        vm.selectedProductIds = selectedProductIds;
+        vm.productDetails = productDetails;
+        vm.productNames = productNames;
+        vm.cumulative = cumulative;
+        vm.users = users;
+        vm.userRanking = userRanking;
         
-        // processes about time
-        let d = new Date(last.time);
-        vm.rightNow = `${d.getDate()} ${d.getHours()}:${d.getMinutes()}`;
-        vm.myChart.data.labels.push(`${d.getHours()}:${d.getMinutes()}`);
+        let time = new Date(vm.cumulative[vm.cumulative.length - 1].time);
+        vm.myChart.data.labels.push(`${time.getHours()}:${time.getMinutes()}`);
 
-        // processes about productDetails
-        vm.productDetails = vm.productDetails.map(element => {
-            if (element.id === vm.productNames.length - 1) {
-                element.sales = putTogether(last.data, 'sales');
-                element.nums = putTogether(last.data, 'nums');
-            } else {
-                element.sales = last.data[element.id].sales;
-                element.nums = last.data[element.id].nums;
-            }
-        });
-
-        // processes about data of myChart
         vm.myChart.data.datasets = vm.myChart.data.datasets.map(element => {
             let productId = vm.productNames.indexOf(element.label);
             element.data.push(vm.productDetails[productId].sales);
